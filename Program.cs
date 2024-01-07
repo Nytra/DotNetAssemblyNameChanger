61c75f3c6674c8dfa0b3539893da6b03a8007474
Nytra <14206961+Nytra@users.noreply.github.com>
2024-01-07 11:19:59 +0000

    Better text feedback and check for valid paths

--- Program.cs
+++ Program.cs
@@ -4,44 +4,106 @@ using System.Reflection;
 Console.WriteLine("AssemblyPostProcess\n");
 
 string executingAssemblyLocation = Assembly.GetExecutingAssembly().Location;
-string executingAssemblyDirectory = Path.GetDirectoryName(executingAssemblyLocation);
+string? executingAssemblyDirectory = Path.GetDirectoryName(executingAssemblyLocation);
 Console.WriteLine("Executing assembly directory: " + executingAssemblyDirectory);
 
 Random rand = new Random();
-
-string assemblyPath, writeDirectory;
+string? assemblyPath = null;
+string? writeDirectory = null;
+bool interactive = false;
+bool invalidArgs = false;
 
 if (args.Length == 0)
 {
-    Console.Write("Input assembly path: ");
-    assemblyPath = Console.ReadLine();
-    Console.Write("Input directory to write the processed assembly to: ");
-    writeDirectory = Console.ReadLine();
+    interactive = true;
+    TakeInput();
 }
-else if (args.Length == 2)
+else if (args.Length >= 1)
 {
-    assemblyPath = args[0].Replace("\"", "");
-    writeDirectory = args[1].Replace("\"", "");
+    if (args[0] == "-i")
+    {
+        interactive = true;
+        TakeInput();
+    }
+    else
+    {
+        assemblyPath = args[0].Replace("\"", "");
+        if (args.Length >= 2)
+        {
+            if (args[1] == "-i")
+            {
+                interactive = true;
+            }
+            else
+            {
+                writeDirectory = args[1].Replace("\"", "");
+                if (args.Length >= 3)
+                {
+                    if (args[2] == "-i")
+                    {
+                        interactive = true;
+                    }
+                    else
+                    {
+                        invalidArgs = true;
+                    }
+                }
+            }
+        }
+    }
 }
 else
 {
-    throw new Exception("Expected 2 arguments! InputAssemblyPath and OutputDirectory path");
+    invalidArgs = true;
+}
+
+if (invalidArgs)
+{
+    throw new Exception("Invalid arguments! Expected: InputAssemblyPath and OutputDirectoryPath. Optional: -i (Interactive mode)");
 }
 
 if (string.IsNullOrWhiteSpace(assemblyPath))
 {
-    throw new Exception("Assembly path cannot be null!");
+    throw new Exception("Assembly path cannot be null or whitespace!");
 }
 
 if (string.IsNullOrWhiteSpace(writeDirectory))
 {
     writeDirectory = null;
+    //interactive = true;
 }
 
-Console.WriteLine("Assembly path: " + assemblyPath);
-Console.WriteLine("Output directory path: " + writeDirectory ?? executingAssemblyDirectory);
+if (!Path.Exists(assemblyPath))
+{
+    throw new Exception("Input assembly file does not exist!");
+}
+
+if (!Path.Exists(writeDirectory))
+{
+    Console.WriteLine();
+    Console.WriteLine("Output directory does not exist! Output file will now be written to the location of the executing assembly.");
+    writeDirectory = executingAssemblyDirectory;
+}
+
+assemblyPath = Path.GetFullPath(assemblyPath);
+
+Console.WriteLine();
+
+Console.WriteLine("Actual Assembly path: " + assemblyPath);
+Console.WriteLine("Actual Output directory path: " + writeDirectory);
+
+Console.WriteLine();
 
 PostProcessAssembly(assemblyPath);
+
+void TakeInput()
+{
+    Console.WriteLine();
+    Console.Write("Input assembly path: ");
+    assemblyPath = Console.ReadLine();
+    Console.Write("Output directory path: ");
+    writeDirectory = Console.ReadLine();
+}
 
 string GetRandomString(int charCount)
 {
@@ -66,24 +128,41 @@ void PostProcessAssembly(string assemblyPath)
 
     Console.WriteLine("Loaded module: " + module.Name);
 
-    Console.WriteLine("Assembly name: " + module.Assembly.Name.ToString());
+    Console.WriteLine("Assembly name definition: " + module.Assembly.Name.ToString());
 
-    Console.WriteLine("\nProcessing public types...\n");
+    Console.WriteLine("\nProcessing public types...");
+
+    int changedTypesCount = 0;
 
     foreach(TypeDefinition typeDef in module.Types)
     {
         if (typeDef.IsNotPublic) continue;
+        Console.WriteLine();
         Console.WriteLine("Found type: " + typeDef.FullName);
         Console.WriteLine("Changing type name...");
         typeDef.Name = typeDef.Name + GetRandomString(32);
         Console.WriteLine("New type name: " + typeDef.FullName);
+        changedTypesCount += 1;
     }
 
-    Console.WriteLine();
+    if (changedTypesCount == 0)
+    {
+        Console.WriteLine("No public types found.\n");
+    }
+    else
+    {
+        Console.WriteLine();
+    }
 
     module.Assembly.Name.Name += GetRandomString(32);
 
     Console.WriteLine("Changed assembly name to: " + module.Assembly.Name.Name);
+
+    Console.WriteLine();
+
+    Console.WriteLine("New assembly name definition: " + module.Assembly.Name.ToString());
+
+    Console.WriteLine();
 
     module.Name = "Processed_" + module.Name;
 
@@ -100,4 +179,10 @@ void PostProcessAssembly(string assemblyPath)
     Console.WriteLine("Writing module to path: " + writePath);
 
     module.Write(writePath);
+}
+
+if (interactive)
+{
+    Console.Write("\nPress enter to quit.");
+    Console.ReadLine();
 }
